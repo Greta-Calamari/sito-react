5e9fcb703f9ddc7a23abd09f6b731851a522f88f
ladykakki <greta_calamari@hotmail.it>
2022-08-24 10:53:13 +0200

    app sushi react

---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+function Navbar(){
+    return(
+    <nav className="navbar-dark bg-dark">
+        <div className="container-fluid">
+            <span className="navbar-brand mb-0 h1">FullStack Sushi</span>
+        </div>
+    </nav>
+    );
+}
+export default Navbar;
